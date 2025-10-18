9ea4c378b553dd97814c93a1c184cfa536f458ed
giulange <gyuliano@libero.it>
2025-10-18 07:23:11 +0000

    Deploying to gh-pages from @ giulange/giulange.github.io@0c7e546c5d4e6eff607425121f0c0ed897083f21 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "giulange/.github.io",
+    "data-repo": "giulange/giulange.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
